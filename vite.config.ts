2515d1e601f7d067623b44d6010663da5fecdfdc
AmirhoseinAKZ <akbarzade@dadehfa.com>
2025-02-11 03:57:38 +0330

    change of build in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/nvidia-ceo/",
+  build: {
+    outDir: "dist", // مشخص کردن مسیر خروجی
+    assetsDir: "assets", // اطمینان از ذخیره درست CSS و JS
+  },
 });
